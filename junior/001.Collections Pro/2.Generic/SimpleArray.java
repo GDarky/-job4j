35d4f3a23a230092088d2ff318b3a9ae7a341125
GDarky <mailto:darky@pisem.net>
2018-09-08 15:40:31 +0300

    5.2.1. Реализовать SimpleArray

---
+++ junior/001.Collections Pro/2.Generic/SimpleArray.java
@@ -0,0 +1,77 @@
+package dstelmachenko.services;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.ParameterizedType;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+
+public class SimpleArray<T> implements Iterable<T>{
+
+    private Object[] array;
+    private int index=0;
+
+    public SimpleArray(int size) {
+        this.array = new Object[size];
+    }
+
+    public void add(T model) {
+        if (index>=array.length)
+            throw new NoSuchElementException("Array out of bounds");
+
+        array[index++]=model;
+        //array = (T[]) Array.newInstance((Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments(), num);
+    }
+
+    public void set(int index, T model)
+    {
+        checkIndex(index);
+        array[index++]=model;
+    }
+
+    public void delete(int index) {
+        checkIndex(index);
+        this.index--;
+        for (int i=index; i<this.index; i++) {
+            array[i]=array[i+1];
+        }
+    }
+
+    public T get(int index) {
+        checkIndex(index);
+        return (T) array[index];
+    }
+
+    private void checkIndex (int index) {
+        if (this.index<index)
+            throw new NoSuchElementException("Array out of bounds");
+    }
+
+    public Iterator<T> iterator() {
+        return new ArrayIterator();
+    }
+
+    class ArrayIterator implements Iterator<T> {
+        int current = 0;
+        public boolean hasNext() {
+            return current<index;
+        }
+
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException("Array out of bounds");
+            }
+            return (T) array[current++];
+        }
+
+        public void remove() {
+            delete(--current);
+        }
+
+        public void replace(T model) {
+            set(current, model);
+        }
+    }
+
+
+}
